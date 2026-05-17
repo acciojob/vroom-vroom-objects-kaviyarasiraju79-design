fcb195fb0863a0c298512bb2bc185ca832c93a34
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-17 22:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit from Car
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset constructor
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
